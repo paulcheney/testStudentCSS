8a5413ff1f165e9f31d380ab0990ea37cd9a909a
Paul Cheney <spartandesign.biz@gmail.com>
2025-10-06 10:24:42 -0600

    test3

--- scripts/date.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const yearEl = document.getElementById('currentyear');
-  if (yearEl) yearEl.textContent = new Date().getFullYear();
-  const lastModifiedEl = document.getElementById('lastModifiedText');
-  if (lastModifiedEl) lastModifiedEl.textContent = document.lastModified || 'Unavailable';
-});
